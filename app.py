aedb58e3e37392bbfa37f2bfe0aa158731fc0921
ArunNandam <naarku30@gmail.com>
2022-09-02 18:44:27 +0530

    the web app is ready

--- app.py
+++ app.py
@@ -40,5 +40,16 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+
+@app.route("/predict",methods=['POST'])
+#creating prediction api when the input is passed over html form and recieve a prediction in that page itself
+def predict():
+    data = [float(x) for x in request.form.values()]
+    transformed_data = scaler.transform(np.array(data).reshape(1,-1))
+    print(transformed_data)
+    output = model.predict(transformed_data)[0]
+    return render_template('home.html', prediction_text = "The house price for the {}".format(output))
+
+    
 if __name__=="__main__":
     app.run(debug=True)
